7e82ab2fa1d0372caf9c0e5f397f4231da599210
wangwhh <2356300214@qq.com>
2023-09-14 20:37:24 +0800

    再试试

--- build/web-mobile/cocos-js/spine.js-f7f5ea79.js
+++ build/web-mobile/cocos-js/spine.js-f7f5ea79.js
@@ -1,1 +1,1 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","https://wangwhh.github.io/CSSU/build/web-mobile/assets/spine.js.mem-1ff7a7d3.bin")}}}));
+System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-1ff7a7d3.bin")}}}));
